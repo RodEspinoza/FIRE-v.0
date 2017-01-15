3351ac317e17ed4c8ca43ae789dbf111b8d3e8a3
novacane <espinoza.sample@gmail.com>
2017-01-15 13:40:21 -0300

    v.0
    public

---
+++ public/main.js
@@ -0,0 +1,8 @@
+
+var config = {
+    apiKey: "AIzaSyDSU_rxx75rmu4lzMrlI-xVO4v4UjKZ02w",
+    authDomain: "sizzling-heat-5310.firebaseapp.com",
+    databaseURL: "https://sizzling-heat-5310.firebaseio.com",
+    storageBucket: "sizzling-heat-5310.appspot.com",
+    messagingSenderId: "1050872571978"
+  };
